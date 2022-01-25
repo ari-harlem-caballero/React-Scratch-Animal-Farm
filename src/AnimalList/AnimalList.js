d6a21afaa471022b0bc917be623f8a8f7efe2001
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2022-01-24 17:53:32 -0800

    added plan to each component

--- src/AnimalList/AnimalList.js
+++ src/AnimalList/AnimalList.js
@@ -2,4 +2,6 @@ import React from 'react';
 
 export default function AnimalList() {
   return <div></div>;
+//   .map loop thru Animals
+//   each Animal needs: name, type, says, img (unique key to prop ea animal)
 }
